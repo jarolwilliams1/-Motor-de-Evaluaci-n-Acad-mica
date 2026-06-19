e492e3d2dc1f915a0d4641d39c60b8202d41b91b
JarolWilliams <215430519+jarolwilliams1@users.noreply.github.com.>
2026-06-18 23:38:30 -0400

    primer commit

---
+++ Motor de Evaluación Académica/ClassLibrary/Evaluacion.cs
@@ -0,0 +1,18 @@
+﻿
+namespace ClassLibrary
+{
+    public abstract class Evaluacion
+    {
+        private  int _nota;
+        public  int Nota { set; get; }
+        public string? NombreEstudiante { set; get; }
+
+        private int _notafinal;
+        public  int CalificacionFinal { private get; set  ; }
+
+
+        public abstract void Evaluar();
+      
+
+    }
+}
---
+++ Motor de Evaluación Académica/ClassLibrary/Examenes.cs
@@ -0,0 +1,33 @@
+﻿
+
+namespace ClassLibrary
+{
+    public class Examenes : Evaluacion
+    {
+
+        private int _nota;
+        public int Nota { set { _nota = value; }
+
+        get{
+                    if( _nota <= 0)
+                {
+                    
+                    throw new Exception("La nota debe de ser mayor a cero");
+
+                }
+                    
+            
+            } }
+
+        public string? NombreEstudiante { set; get; }
+
+        private int _notafinal;
+        public int CalificacionFinal { private get; set { _nota = value; } }
+
+        public override void Evaluar()
+        {
+                        
+
+        }
+    }
+}
